bf4dfa4b5014b8096f0ff2e14d25acecaf3bb6dd
Gert-Jaap Glasbergen <gertjaap@gertjaap.nl>
2018-12-30 19:31:09 +0100

    Lyra2REv3 Testnet and Mainnet (#34)
    * Lyra2REv3 Testnet
    
    * Lyra2REv3 for mainnet

--- p2pool/bitcoin/networks/vertcoin_testnet.py
+++ p2pool/bitcoin/networks/vertcoin_testnet.py
@@ -14,7 +14,7 @@ SEGWIT_ADDRESS_VERSION=196
 RPC_PORT=15888
 RPC_CHECK=lambda bitcoind: True
 SUBSIDY_FUNC=lambda height: 50*100000000 >> (height + 1)//840000
-POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('lyra2re2_hash').getPoWHash(data))
+POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('lyra2re3_hash').getPoWHash(data))
 BLOCK_PERIOD=150 # s
 SYMBOL='VTCTEST'
 CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Vertcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Vertcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.vertcoin'), 'vertcoin.conf')
