30a6a63a03ec5b96c5522510b8c2c6dc0114440a
James Lovejoy <jameslovejoy1@gmail.com>
2021-01-24 18:44:53 -0500

    Add verthash datefile integrity check

--- p2pool/bitcoin/networks/vertcoin.py
+++ p2pool/bitcoin/networks/vertcoin.py
@@ -1,5 +1,6 @@
 import os
 import platform
+import hashlib
 
 from twisted.internet import defer
 
@@ -12,6 +13,9 @@ verthash_data = None
 if verthash_data is None:
     with open('verthash.dat', 'rb') as f:
         verthash_data = f.read()
+
+    verthash_sum = hashlib.sha256(verthash_data).hexdigest()
+    assert verthash_sum == 'a55531e843cd56b010114aaf6325b0d529ecf88f8ad47639b6ededafd721aa48'
 
 def verthash_hash(dat):
     return verthash.getPoWHash(dat, verthash_data)
